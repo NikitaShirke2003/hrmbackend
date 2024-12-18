e9f8a0edf3152828881faf13beb359710a5600f6
NIkitaShirke <nikitashirke195@gmail.com>
2024-12-18 15:40:42 +0530

    Story No:[1234]Added new API as sortByName

--- Springbootwithmongodbcrudex/src/main/java/com/csi/controller/CustomerController.java
+++ Springbootwithmongodbcrudex/src/main/java/com/csi/controller/CustomerController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
@@ -29,4 +30,10 @@ public class CustomerController {
         return ResponseEntity.ok(customerServiceImpl.findAll());
     }
 
+    @GetMapping("/sortbyname")
+
+    public ResponseEntity<List<Customer>>sortByName()
+    {
+        return ResponseEntity.ok(customerServiceImpl.findAll().stream().sorted(Comparator.comparing(Customer::getCustName)).toList());
+    }
 }
